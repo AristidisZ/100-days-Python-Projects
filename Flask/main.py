4e4d95f7944d0da33b39844850dece33c0866112
Aristidis Zotka <aristidiszotka@gmail.com>
2024-07-02 02:06:16 +0300

    Flast tutorial part 1

--- Flask/main.py
+++ Flask/main.py
@@ -1,16 +1,42 @@
 from flask import Flask
-import time
-
-
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello_world():
-    return "Hello world!"
+    return "<h1 style='text-align: center'>Hello, World!</h1>" \
+           "<p>This is a paragraph.</p>" \
+           "<img src='https://media.giphy.com/media/hvS1eKlR75hMr0l7VJ/giphy.gif'>"
 
 
-if __name__ == "__main__" :
-    app.run()
+def make_bold(function):
+    def wrapper():
+        text = function()
+        return f"<b>{text}</b>"
+    return wrapper
+
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+    return wrapper
+
+def make_underlined(function):
+    def wrapper():
+        text = function()
+        return f"<u>{text}</u>"
+    return wrapper
 
 
+@app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underlined
+def say_bye():
+    return "Bye"
+
+@app.route("/<name>/<int:number>")
+def greet(name, number):
+    return f"Hello {name}, you are {number} years old!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
